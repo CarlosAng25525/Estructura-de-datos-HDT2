4c3fa19054495990df1944f51d773219357111bb
maxzadi <maxzadi20@gmail.com>
2026-02-06 19:31:40 -0600

    Vector e interfaz hechas

---
+++ VectorStack.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class VectorStack<T> implements Stack<T> {
+    Vector<T> pila = new Vector<>();
+
+    public void push(T element){
+        pila.addElement(element);
+    };
+
+    public T pop(){
+        T dato = pila.lastElement();
+        pila.removeElementAt(-1);
+        return dato;
+    };
+    public T peek(){
+        return pila.lastElement();
+    };
+
+    public int size(){
+        return pila.size();
+    };
+
+    public boolean isEmpty(){
+        return pila.size()==0;
+    };
+}
